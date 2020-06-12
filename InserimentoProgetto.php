6f587210a15cd33d9d6357e33f043f4ec42100fa
emanuele <emanuelesala15@gmail.com>
2020-06-12 18:29:18 +0200

    Revisione Inserimento progetto

--- InserimentoProgetto.php
+++ InserimentoProgetto.php
@@ -8,7 +8,14 @@ $data_scadenza = "";
 $data_creazione = "";
 $creazione_date = "";
 $scadenza_date = "";
-$tag = "";
+
+$tag = array("tag1" => $_POST["tag1"], "tag2" => $_POST["tag2"], "tag3" => $_POST["tag3"]);
+
+if(isset($_POST["tag4"]))
+  $tag["tag4"] = $_POST["tag4"];
+
+if(isset($_POST["tag5"]))
+  $tag["tag5"] = $_POST["tag5"];
 
 
 if(isset($_POST['nome'])){
@@ -25,10 +32,6 @@ if(isset($_POST['data_scadenza'])){
 
 if(isset($_POST['data_creazione'])){
    	$data_creazione = $_POST['data_creazione'];
-}
-
-if(isset($_POST['tag'])){
-    $tag = $_POST['tag'];
 }
 
 
@@ -64,7 +67,7 @@ if($creazione_date > $scadenza_date){
 </head>
 <body>
 	   <center>
-           <form  ACTION="InserimentoProgetto.php" METHOD = POST>
+           <form  ACTION="InserimentoProgetto.php" onsubmit = "return Prova()" METHOD = POST>
              <table border="1" align="center"> <br>
              	
               <input type="text" id="nome" name="nome" size="30" placeholder="Nome"> <br><br>
@@ -77,10 +80,41 @@ if($creazione_date > $scadenza_date){
               
               <input type="date" id="date" name="data_scadenza" placeholder="Data creazione"> <br><br>
 
-              <input type="text" id="tag" name="tag" placeholder="Tag"> <br><br>
+              <input type="text" id="tag1" name="tag1" placeholder="Tag1"> <br><br>
+              <input type="text" id="tag2" name="tag2" placeholder="Tag2"> <br><br>
+              <input type="text" id="tag3" name="tag3" placeholder="Tag3"> <br><br> 
 
               <input  type="submit" align="right" id="login_btn" value="Inserisci"> <br><br>
-        	
+
+              <script type="text/javascript">
+              
+              function Prova() {
+
+                var tag1 = document.getElementById('tag1').value;
+                var tag2 = document.getElementById('tag2').value;
+                var tag3 = document.getElementById('tag3').value;
+
+                if(tag1 == ""){
+                    alert("Il campo tag1 è obbligatorio.");
+                    document.getElementById('tag1').focus();
+                    return false;
+                }
+
+                if(tag2 == ""){
+                    alert("Il campo tag2 è obbligatorio.");
+                    document.getElementById('tag2').focus();
+                    return false;
+                }
+
+                if(tag3 == ""){
+                    alert("Il campo tag3 è obbligatorio.");
+                    document.getElementById('tag3').focus();
+                    return false;
+                }
+                return true;
+            } 
+              </script>
+        
         	</table>
            </form>
        </center>
