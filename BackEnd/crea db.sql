33db59fa6d0ff61095c17b0faa1ea654bd656a24
emanuele <emanuelesala15@gmail.com>
2020-06-26 17:44:36 +0200

    Cancella progetto e revisione db

--- BackEnd/crea db.sql
+++ BackEnd/crea db.sql
@@ -5,9 +5,9 @@ CREATE TABLE progetto (ID int NOT NULL AUTO_INCREMENT, leader varchar(60) NOT NU
 CREATE TABLE utente (mail varchar(60) NOT NULL, password varchar(64) , nome varchar(40) NOT NULL, cognome varchar(50) NOT NULL, nascita date, descrizione varchar(250) NOT NULL, immagine varchar(255), curriculum varchar(255), PRIMARY KEY (mail));
 CREATE TABLE tag (nome varchar(60) PRIMARY KEY);
 CREATE TABLE associaTag (progetto int, tag varchar(60), posizione int, PRIMARY KEY (progetto, tag, posizione));
-ALTER TABLE associaTag ADD CONSTRAINT FKassociaTag524966 FOREIGN KEY (Progetto) REFERENCES progetto (ID);
+ALTER TABLE associaTag ADD CONSTRAINT FKassociaTag524966 FOREIGN KEY (Progetto) REFERENCES progetto (ID) ON DELETE CASCADE;
 ALTER TABLE associaTag ADD CONSTRAINT FKassociaTag918278 FOREIGN KEY (tag) REFERENCES tag (nome);
-ALTER TABLE candidato ADD CONSTRAINT FKcandidato524966 FOREIGN KEY (Progetto) REFERENCES progetto (ID);
-ALTER TABLE candidato ADD CONSTRAINT FKcandidato918278 FOREIGN KEY (utente) REFERENCES utente (mail);
-ALTER TABLE progetto ADD CONSTRAINT FKProgetto579424 FOREIGN KEY (Leader) REFERENCES Utente (mail);
+ALTER TABLE candidato ADD CONSTRAINT FKcandidato524966 FOREIGN KEY (Progetto) REFERENCES progetto (ID) ON DELETE CASCADE;
+ALTER TABLE candidato ADD CONSTRAINT FKcandidato918278 FOREIGN KEY (utente) REFERENCES utente (mail) ON DELETE CASCADE;
+ALTER TABLE progetto ADD CONSTRAINT FKProgetto579424 FOREIGN KEY (Leader) REFERENCES Utente (mail) ON DELETE CASCADE;
 ALTER TABLE associaTag ADD CONSTRAINT limitToFive CHECK (posizione < 5 AND posizione >= 0);
